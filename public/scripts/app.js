953a6ec661d3488c9f6baaffcec2d5dd3166a367
Mason Galland <masongalland@users.noreply.github.com>
2017-03-13 17:39:14 -0600

    day 1 done

---
+++ public/scripts/app.js
@@ -0,0 +1,18 @@
+angular.module('happyGoMarry', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './html/home/home.html'
+            })
+            .state('howItWorks',{
+                url: '/how-it-works',
+                templateUrl: './html/howItWorks/howItWorks.html'
+            })
+            .state('createPage',{
+                url: '/create-page',
+                templateUrl: '/html/createPage/createPage.html',
+                controller: '/scripts/createPage/createPageCtrl.js'
+            });
+    })
---
+++ public/scripts/mainCtrl.js
@@ -0,0 +1,4 @@
+angular.module("happyGoMarry").controller("mainCtrl", function($scope){
+   
+
+});
