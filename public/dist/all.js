31c87d3c64ebf7b6644eda2e3181e476766dbc90
Mason Galland <masongalland@users.noreply.github.com>
2017-03-27 15:12:49 -0600

    pre-presentation

--- public/dist/all.js
+++ public/dist/all.js
@@ -474,36 +474,139 @@ angular.module('happyGoMarry').directive('signupControls', function () {
         link: function link(scope, element, attributes) {
             $(document).ready(function () {
                 $('#welcome-btn').click(function () {
-                    $('#welcome-message').css('display', 'none');
-                    $('#question1').css('display', 'block');
+
+                    $('#welcome-message').addClass('animated fadeOutLeft');
+                    $('#welcome-message').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#welcome-message').removeClass('animated fadeOutLeft');
+                    });
+                    $('#question1').addClass('animated slideInRight');
+                    $('#question1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question1').removeClass('animated slideInRight');
+                    });
+                    setTimeout(function () {
+                        $('#welcome-message').css('display', 'none');
+                        $('#question1').css('display', 'block');
+                    }, 500);
                 });
                 $('#name-next-btn').click(function () {
-                    $('#question1').css('display', 'none');
-                    $('#question2').css('display', 'block');
+
+                    $('#question1').addClass('animated fadeOutLeft');
+                    $('#question1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question1').removeClass('animated fadeOutLeft');
+                    });
+                    $('#question2').addClass('animated slideInRight');
+                    $('#question2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question2').removeClass('animated slideInRight');
+                    });
+
+                    setTimeout(function () {
+                        $('#question1').css('display', 'none');
+                        $('#question2').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////
                 $('#details-back-btn').click(function () {
-                    $('#question2').css('display', 'none');
-                    $('#question1').css('display', 'block');
+
+                    $('#question2').addClass('animated fadeOutRight');
+                    $('#question2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question2').removeClass('animated fadeOutRight');
+                    });
+                    $('#question1').addClass('animated slideInLeft');
+                    $('#question1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question1').removeClass('animated slideInLeft');
+                    });
+
+                    setTimeout(function () {
+                        $('#question2').css('display', 'none');
+                        $('#question1').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////
+
+
                 $('#details-next-btn').click(function () {
-                    $('#question2').css('display', 'none');
-                    $('#question3').css('display', 'block');
+
+                    $('#question2').addClass('animated fadeOutLeft');
+                    $('#question2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question2').removeClass('animated fadeOutLeft');
+                    });
+                    $('#question3').addClass('animated slideInRight');
+                    $('#question3').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question3').removeClass('animated slideInRight');
+                    });
+
+                    setTimeout(function () {
+                        $('#question2').css('display', 'none');
+                        $('#question3').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////                
                 $('#photo-back-btn').click(function () {
-                    $('#question3').css('display', 'none');
-                    $('#question2').css('display', 'block');
+
+                    $('#question3').addClass('animated fadeOutRight');
+                    $('#question3').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question3').removeClass('animated fadeOutRight');
+                    });
+                    $('#question2').addClass('animated slideInLeft');
+                    $('#question2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question2').removeClass('animated slideInLeft');
+                    });
+
+                    setTimeout(function () {
+                        $('#question3').css('display', 'none');
+                        $('#question2').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////                
                 $('#photo-next-btn').click(function () {
-                    $('#question3').css('display', 'none');
-                    $('#question4').css('display', 'block');
+
+                    $('#question3').addClass('animated fadeOutLeft');
+                    $('#question3').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question3').removeClass('animated fadeOutLeft');
+                    });
+                    $('#question4').addClass('animated slideInRight');
+                    $('#question4').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question4').removeClass('animated slideInRight');
+                    });
+
+                    setTimeout(function () {
+                        $('#question3').css('display', 'none');
+                        $('#question4').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////                                
                 $('#story-back-btn').click(function () {
-                    $('#question4').css('display', 'none');
-                    $('#question3').css('display', 'block');
+
+                    $('#question4').addClass('animated fadeOutRight');
+                    $('#question4').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question4').removeClass('animated fadeOutRight');
+                    });
+                    $('#question3').addClass('animated slideInLeft');
+                    $('#question3').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question3').removeClass('animated slideInLeft');
+                    });
+
+                    setTimeout(function () {
+                        $('#question4').css('display', 'none');
+                        $('#question3').css('display', 'block');
+                    }, 500);
                 });
+                ////////back button//////////                                
                 $('#story-next-btn').click(function () {
-                    $('#question4').css('display', 'none');
-                    $('#verify-url').css('display', 'block');
+
+                    $('#question4').addClass('animated fadeOutLeft');
+                    $('#question4').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#question4').removeClass('animated fadeOutLeft');
+                    });
+                    $('#verify-url').addClass('animated slideInRight');
+                    $('#verify-url').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        $('#verify-url').removeClass('animated slideInRight');
+                    });
+
+                    setTimeout(function () {
+                        $('#question4').css('display', 'none');
+                        $('#verify-url').css('display', 'block');
+                    }, 500);
                 });
                 $('#url-back-btn').click(function () {
                     $('#verify-url').css('display', 'none');
