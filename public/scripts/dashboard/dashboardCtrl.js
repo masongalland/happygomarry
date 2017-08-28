06030f84ac3bfcd5c953d6c715c7d89ba2e2a1d6
Mason Galland <masongalland@me.com>
2017-08-28 11:40:54 -0600

    made dashboard page load correct data no matter where you access it from

--- public/scripts/dashboard/dashboardCtrl.js
+++ public/scripts/dashboard/dashboardCtrl.js
@@ -1,10 +1,9 @@
 angular.module('happyGoMarry')
 .controller('dashboardCtrl', function($scope, coupleSrv){
-
     coupleSrv.getUser()
     .then(function(response){
         $scope.couple = response;  
-        console.log('dash couple: ', $scope.couple)
+        console.log('dashboard couple: ', $scope.couple)
         $scope.url = response.url;
 
         return coupleSrv.getPayments($scope.couple.userid)
@@ -12,49 +11,51 @@ angular.module('happyGoMarry')
     .then(function(response){
         $scope.gifts = response.data;
         console.log('dash gifts: ', $scope.gifts);
+        console.log('scope.couple: ', $scope.couple);
+
+        coupleSrv.getDonations().then(function(response){
+            $scope.donations = response.data[0];
+            console.log($scope.donations);
+        })
+        coupleSrv.getAddresses($scope.couple.userid).then(function(response){
+            $scope.addresses = response.data;
+            console.log($scope.addresses);
+        })
+        coupleSrv.getRsvps($scope.couple.userid).then(function(response){
+            $scope.guests = response.data;
+            console.log($scope.guests);
+        })
+        setTimeout(function(){
+            $scope.userUpdates = {
+            firstName: $scope.couple.firstname,
+            partnerFirstName: $scope.couple.partnerfirstname,
+            photoUrl: $scope.couple.photourl,
+            story: $scope.couple.story,
+            hour: $scope.couple.hour,
+            place: $scope.couple.place,
+            userId: $scope.couple.userid,
+            weddingDate: $scope.couple.weddingDate
+            }
+        }, 100) 
+        $scope.saveUpdatedCouple = function(userUpdates) {
+            coupleSrv.saveUpdatedCouple(userUpdates).success(function() { 
+                swal(
+                    'Thanks!',
+                    'Your page has been updated',
+                    'success'
+                ); 
+            }).error(function(){
+                swal(
+                    'Oops...',
+                    'Something went wrong!',
+                    'error'
+                );
+            });   
+            console.log('userupdates', $scope.userUpdates)
+            console.log('updatedCouple', $scope.couple)
+        }
     })
 
-    coupleSrv.getDonations().then(function(response){
-        $scope.donations = response.data[0];
-        console.log($scope.donations);
-    })
-    coupleSrv.getAddresses($scope.couple.userid).then(function(response){
-        $scope.addresses = response.data;
-        console.log($scope.addresses);
-    })
-    coupleSrv.getRsvps($scope.couple.userid).then(function(response){
-        $scope.guests = response.data;
-        console.log($scope.guests);
-    })
-    setTimeout(function(){
-        $scope.userUpdates = {
-        firstName: $scope.couple.firstname,
-        partnerFirstName: $scope.couple.partnerfirstname,
-        photoUrl: $scope.couple.photourl,
-        story: $scope.couple.story,
-        hour: $scope.couple.hour,
-        place: $scope.couple.place,
-        userId: $scope.couple.userid,
-        weddingDate: $scope.couple.weddingDate
-        }
-    }, 100) 
-    $scope.saveUpdatedCouple = function(userUpdates) {
-        coupleSrv.saveUpdatedCouple(userUpdates).success(function() { 
-            swal(
-                'Thanks!',
-                'Your page has been updated',
-                'success'
-            ); 
-        }).error(function(){
-            swal(
-                'Oops...',
-                'Something went wrong!',
-                'error'
-            );
-        });   
-        console.log('userupdates', $scope.userUpdates)
-        console.log('updatedCouple', $scope.couple)
-    }
 
 
     // setInterval(function() {
